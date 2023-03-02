adc6034f9a35212fd33d0e6614d9ae9cb69def00
Do Duc Duong <duongdd2@vingroup.net>
2023-03-02 17:30:03 +0700

    conditional redering

---
+++ conditional-redering/src/views/Users.vue
@@ -0,0 +1,41 @@
+<script>
+    export default {
+        data() {
+            return {
+                users: [
+                    {
+                        id: 1,
+                        name: 'duongdd',
+                        isActive: true
+                    },
+                    {
+                        id: 2,
+                        name: 'hungpv',
+                        isActive: true
+                    },
+                    {
+                        id: 3,
+                        name: 'hieupm',
+                        isActive: false
+                    }
+                ]
+            }
+        }
+    }
+</script>
+<template>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>active</th>
+        </tr>
+        <template v-for="user in users" :key="user.id">
+            <tr v-if="user.isActive">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.isActive }}</td>
+            </tr>
+        </template>
+    </table>
+</template>

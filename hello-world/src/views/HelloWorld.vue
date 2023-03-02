33672913704e5ced8225dc558c26b19ad5c0161d
Do Duc Duong <duongdd2@vingroup.net>
2023-03-02 14:41:04 +0700

    Hello Vue

---
+++ hello-world/src/views/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        data() {
+            return {
+                count: 10,
+                isHidden: true
+            }
+        },
+        methods: {
+            countDown() {
+                this.count = this.count == 0 ? this.count : this.count - 1;
+                if (this.count == 0) {
+                    this.isHidden = false;
+                }
+            }
+
+        }
+    }
+</script>
+<template>
+    <div ref="home1" id="home">
+        <button @click="countDown">{{ count }}</button>
+        <p ref="text" :hidden="isHidden">Happy New Year!!!</p>
+    </div>
+</template>

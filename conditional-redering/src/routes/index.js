cf2415d220fe1bac8a94544b677aa2b320457a84
Do Duc Duong <duongdd2@vingroup.net>
2023-03-03 09:17:47 +0700

    Loop Object

--- conditional-redering/src/routes/index.js
+++ conditional-redering/src/routes/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Users from '../views/Users.vue'
+import LoopObject from '../views/LoopObject.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
             path: '/',
             name: 'users',
             component: Users
+        },
+        {
+            path: '/loop-object',
+            name: 'loop object',
+            component: LoopObject
         }
     ]
 })
---
+++ conditional-redering/src/views/LoopObject.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        data() {
+            return {
+                objects: {
+                    key1: 'value1',
+                    key2: 'value2',
+                    key3: 'value3'
+                }
+            }
+        }
+    }
+</script>
+<template>
+    <li v-for="(value, key) in objects">
+        <span>{{ key }}: {{ value }}</span>
+    </li>
+</template>
